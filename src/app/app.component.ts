48d55d2d6a977956f4faf780fe9c62f91bfe0f68
Github Projects <barik011@gmail.com>
2024-09-27 10:34:50 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cc-class1';
+
+  //Text Interpolation and Attribute Binding
+  buttonText:string = 'Click me!';
+
+  //Attribute Binding
+  isDisbaled:boolean = true;
+  imgSrc:string = 'https://angular.io/assets/images/logos/angular/angular.svg';
+
+  // Style Binding
+  bgColor:string ='lightblue';
+  textColor:string = 'orange';
+  fz30:string = '30px';
+  FZ20:string = '20px';
+  multiProperty:string = 'background-color:lightgreen;color:black;font-size:16px;';
+
+   // Class Binding
+  addClass:boolean=true;
+
+  addNewClassess:string = 'yes';
+
+  // Event Binding
+  count:number=0;
+  onIncrementHandler(){
+    this.count++;
+  }
+
+  // Two way Binding
+  inputValue:string='Initial Value';
+
+
+  // NgClass Binding
+  cadrProperty:string='cardProperty border';
+
+  // NgStyle Binding
+  redColor:string='lightgreen';
+
 }
